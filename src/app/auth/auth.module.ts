39bf56b81cdc5a492d89d21c218c51e219c462ac
Darko Draskovic <darko.draskovic@gmail.com>
2020-12-07 13:03:35 +0100

    Remove redundant auth routing module
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,19 +1,11 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AuthRoutingModule } from './auth-routing.module';
 import { NbAuthModule } from '@nebular/auth';
 import { NbAlertModule, NbButtonModule, NbCheckboxModule, NbInputModule } from '@nebular/theme';
 
 import { LoginComponent } from './login/login.component';
-
 
 @NgModule({
   imports: [
@@ -23,7 +15,6 @@ import { LoginComponent } from './login/login.component';
     NbInputModule,
     NbButtonModule,
     NbCheckboxModule,
-    AuthRoutingModule,
 
     NbAuthModule,
   ],
